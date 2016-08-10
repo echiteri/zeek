a99fd93d28bd2dae9c8ead0437c8d7bde429c62c
Stephen S. Musoke <ssmusoke@gmail.com>
2016-08-10 18:11:54 +0300

    Validation for phone numbers at registration

--- omod/src/main/webapp/resources/javascript/namibia.js
+++ omod/src/main/webapp/resources/javascript/namibia.js
@@ -1,3 +1,19 @@
 /**
  * Custom Javascript functions for Namibia Implementation
+ *
  */
+jq(document).ready(function () {
+
+    /* Add validation rule for Namibia phone numbers, once applied to an element will validate the format and show a message
+     */
+    jq.validator.addMethod("namibiaphone", function( phone_number, element ) {
+        phone_number = phone_number.replace( /\(|\)|\s+|-/g, "" );
+        return this.optional( element ) || phone_number.length == 10 &&
+                                           phone_number.match( /^[0-9]{1,10}$/ );
+    }, "Please specify a valid mobile number with 10 digits without any spaces" );
+
+    /* Set validaton to the registration form */
+    jq('#registration').validate();
+});
+
+
