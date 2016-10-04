1de2bb8ff9e59170fa3878cc8e56f5c18aa479db
Stephen S. Musoke <ssmusoke@gmail.com>
2016-10-04 21:14:59 +0300

    Updated to flow from ART initiation algorithm

--- omod/src/main/webapp/resources/javascript/namibia.js
+++ omod/src/main/webapp/resources/javascript/namibia.js
@@ -2,18 +2,23 @@
  * Custom Javascript functions for Namibia Implementation
  *
  */
-jq(document).ready(function () {
-
-    /* Add validation rule for Namibia phone numbers, once applied to an element will validate the format and show a message
-     */
-    jq.validator.addMethod("namibiaphone", function( phone_number, element ) {
-        phone_number = phone_number.replace( /\(|\)|\s+|-/g, "" );
-        return this.optional( element ) || phone_number.length == 10 &&
-                                           phone_number.match( /^[0-9]{1,10}$/ );
-    }, "Please specify a valid mobile number with 10 digits without any spaces" );
-
-    /* Set validaton to the registration form */
-    jq('#registration').validate();
-});
-
-
+/*
+ * Hide the container, and disable all elements in it
+ *
+ * @param the Id of the container
+ */
+function hideContainer(container) {
+    jq(container).addClass('hidden');
+    jq(container + ' :input').attr('disabled', true);
+    jq(container + ' :input').prop('checked', false);
+}
+/*
+ * Show the container, and enable all elements in it
+ *
+ * @param the Id of the container
+ */
+function showContainer(container) {
+    jq(container).removeClass('hidden');
+    jq(container + ' :input').attr('disabled', false);
+    jq(container + ' :input').prop('checked', false);
+}
