ea69a4065c9994d159aa0f8c1f6f3976c7fd1683
Stephen S. Musoke <ssmusoke@gmail.com>
2017-04-03 17:56:36 +0300

    Moved the encounter types from the PMTCT_Encounter_Types-1.zip to a class to have more control over naming.

---
+++ api/src/main/java/org/openmrs/module/namibia/deploy/bundle/NamibiaEncounterTypeMetadataBundle.java
@@ -0,0 +1,20 @@
+package org.openmrs.module.namibia.deploy.bundle;
+
+import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import org.openmrs.module.namibia.metadata.EncounterTypes;
+import org.springframework.stereotype.Component;
+
+/**
+ * Deploys encounter types
+ */
+@Component
+public class NamibiaEncounterTypeMetadataBundle extends AbstractMetadataBundle {
+	
+	@Override
+	public void install() throws Exception {
+		install(EncounterTypes.ANC_ENCOUNTER_TYPE);
+		install(EncounterTypes.LABOR_AND_DELIVERY_ENCOUNTER_TYPE);
+		install(EncounterTypes.MOTHER_PNC_ENCOUNTER_TYPE);
+		install(EncounterTypes.INFANT_PNC_ENCOUNTER_TYPE);
+	}
+}
