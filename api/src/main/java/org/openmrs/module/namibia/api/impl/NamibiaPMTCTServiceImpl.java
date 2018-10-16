0e9908ff03a48be467aee37dd72715e8f52e1efe
Ezekiel Chiteri <ezekiel.chiteri@ucglobalprograms.orggit>
2018-10-16 16:41:42 +0300

    Completed task creation to automatically add PTracker ID for new pregnancy 1. created the task on the databas in liquibase 2. implemented the interface for creating the new Ptracker ID

--- api/src/main/java/org/openmrs/module/namibia/api/impl/NamibiaPMTCTServiceImpl.java
+++ api/src/main/java/org/openmrs/module/namibia/api/impl/NamibiaPMTCTServiceImpl.java
@@ -7,16 +7,32 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
+
 package org.openmrs.module.namibia.api.impl;
 
+import java.util.List;
+import java.util.Iterator;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.type.IdentifierType;
+import org.openmrs.PatientIdentifierType;
 import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.namibia.api.NamibiaPMTCTService;
 import org.openmrs.module.namibia.api.db.NamibiaPMTCTDAO;
 import org.openmrs.Patient;
+import org.openmrs.api.PatientService;
 import org.openmrs.PatientIdentifier;
 import org.openmrs.Person;
+import org.openmrs.api.PersonService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.namibia.api.NamibiaPMTCTService;
+import org.openmrs.module.namibia.api.db.NamibiaPMTCTDAO;
+import org.openmrs.module.namibia.metadata.PatientIdentifierTypes;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 /**
  * It is a default implementation of {@link NamibiaPMTCTService}.
@@ -26,6 +42,9 @@ public class NamibiaPMTCTServiceImpl extends BaseOpenmrsService implements Namib
 	protected final Log log = LogFactory.getLog(this.getClass());
 	
 	private NamibiaPMTCTDAO dao;
+
+	@Autowired
+	private PatientService patientService;
 	
 	/**
 	 * @return the dao
@@ -42,25 +61,29 @@ public class NamibiaPMTCTServiceImpl extends BaseOpenmrsService implements Namib
 	}
 
 	public void createNewPregnancyPTrackerID (Integer patient_id, String ptracker_id) {
-		log.debug("Creating a new pregnancy PTracker ID " + patient_id + " for patient ID " + ptracker_id);
-		// find the mother by identifier
-		/*List<Patient> mothers = patientService.getPatients(null, // name of the person
-				motherARTNumber, //mother ART number
-				Arrays.asList(Context.getPatientService().getPatientIdentifierTypeByUuid(
-						PatientIdentifierTypes.HIV_CARE_NUMBER.uuid())), // ART Number Identifier type
-				true); // match Identifier exactly
-		if (mothers.size() != 0) {
-			Person potentialMother = mothers.get(0).getPerson();
-			// mothers have to be female and above 12 years of age
-			if (potentialMother.getAge() > 12 & potentialMother.getGender().equals("F")) {
-				Relationship relationship = new Relationship();
-				relationship.setRelationshipType(
-						personService.getRelationshipTypeByUuid("8d91a210-c2cc-11de-8d13-0010c6dffd0f"));
-				relationship.setPersonA(potentialMother);
-				relationship.setPersonB(infant);
-				personService.saveRelationship(relationship);
-				log.debug("Infant with ID " + infant.getPersonId() + " linked to mother with ID " + potentialMother.getPersonId());
+		List<PatientIdentifierType> identifierTypes = patientService.getPatientIdentifierTypes(null, null, null, null);
+		PatientIdentifier patientIdentifier = new PatientIdentifier();
+		Patient patient = patientService.getPatient(patient_id);
+
+		for (int i = 7  ; i < 14 ; i++) /* 8 new ptracker id types TODO: optimize the iteration to be durable instead of hardcoding */ {
+
+			try {
+				if (!patient.getIdentifiers().toString().contains(ptracker_id) && patient.getPatientIdentifier(identifierTypes.get(i-1)) == null) {
+					log.debug("Creating a new pregnancy PTracker ID " + patient_id + " for patient ID " + ptracker_id);
+					patientIdentifier.setIdentifier(ptracker_id); // identifier
+					patientIdentifier.setIdentifierType(patientService.getPatientIdentifierTypeByUuid(identifierTypes.get(i-1).getUuid())); // OpenMRS ID with check digit
+					patientIdentifier.setPreferred(false);
+					patientIdentifier.setCreator(Context.getUserService().getUser(1));
+					patientIdentifier.setDateCreated(new Date());
+					patientIdentifier.setVoided(false);
+					patientIdentifier.setUuid(UUID.randomUUID().toString());
+					patient.addIdentifier(patientIdentifier);
+					patientService.savePatient(patient);
+					break;
+				}
+			} catch (NullPointerException e) {
+				System.out.print("Caught NullPointerException");
 			}
-		}*/
+		}
 	}
 }
